36af96d333b44da63e00ccb54f17bafb5a72e983
geekbrains <chester_095@mail.ru>
2021-12-09 12:04:44 +0300

    Change Date

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Timofeev SS  -  02.11.2021
+# Timofeev SS  -  08.12.2021
 ## **FilmLibrary** - An application for finding movies, according to which all practical tasks will be given.
 ---
 ### Functionality:
--- app/src/main/java/com/chester095/filmlibrary/MainActivity.kt
+++ app/src/main/java/com/chester095/filmlibrary/MainActivity.kt
@@ -2,6 +2,7 @@ package com.chester095.filmlibrary
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +10,5 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+class Films(var movieId: Int, var movieTitle: String, var movieCreationDate: Date, var movieProducerCountry: String)
